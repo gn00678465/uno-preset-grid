b4d25a3bc6aaf2798396addb31c92d05e8c96709
Madao <gn00678465@gmail.com>
2025-01-07 05:51:35 +0800

    fix: 修正產生 responsive container 錯誤

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { definePreset } from "unocss"
+import { toEscapedSelector as e } from 'unocss'
+import type { Preset } from 'unocss'
 import type { Theme } from "unocss/preset-mini"
 import type { GridOptions } from "./type"
 import { convertBreakpointSize } from './utils'
 
-export const presetGrid = definePreset((options: GridOptions = {}) => {
+export function presetGrid (options: GridOptions = {}): Preset {
   const columns = options?.columns ?? 12
   const gutter = options?.gutter ?? 24
   const variablePrefix = options?.variablePrefix ?? 'un-'
@@ -17,56 +18,75 @@ export const presetGrid = definePreset((options: GridOptions = {}) => {
 
   return {
     name: 'uno-preset-grid',
-    preflights: [
-      {},
-    ],
     rules: [
       [
         /^flex-container$/,
-        function*(_, { symbols, generator }) {
+        function(_, { generator, rawSelector }) {
+          const _selector = e(rawSelector)
           const _breakpoints = (generator?.userConfig?.theme as Theme)?.breakpoints ?? breakpoints
           const _breakpointEntries = Object.entries(_breakpoints)
             .map(([k, v]) => [k, convertBreakpointSize(v)])
             .filter(([, v]) => !!v)
             .sort(([, a], [, b]) => (b as number) - (a as number))
 
-          yield {
-            [`--${variablePrefix}gutter-x`]: `${gutter / 2}px`,
-            [`--${variablePrefix}gutter-y`]: '0px',
-            width: '100%',
-            'padding-right': `var(--${variablePrefix}gutter-x, .75rem)`,
-            'padding-left': `var(--${variablePrefix}gutter-x, .75rem)`,
-            'margin-right': 'auto',
-            'margin-left': 'auto'
-          }
-          yield {
-            [symbols.selector]: selector => `${selector}-fluid`,
-            [`--${variablePrefix}gutter-x`]: `${gutter / 2}px`,
-            [`--${variablePrefix}gutter-y`]: '0px',
-            width: '100%',
-            'padding-right': `var(--${variablePrefix}gutter-x, .75rem)`,
-            'padding-left': `var(--${variablePrefix}gutter-x, .75rem)`,
-            'margin-right': 'auto',
-            'margin-left': 'auto'
-          }
-
-          for (const [, value] of _breakpointEntries) {
-            yield {
-              [symbols.parent]: `@media (min-width: ${value}px)`,
-              'max-width': value
-              
+            return `
+            ${_selector},
+            ${_selector}-fluid {
+              --${variablePrefix}gutter-x: ${gutter / 2}px;
+              --${variablePrefix}gutter-y: 0px;
+              width: 100%;
+              padding-right: var(--${variablePrefix}gutter-x, .75rem);
+              padding-left: var(--${variablePrefix}gutter-x, .75rem);
+              margin-right: auto;
+              margin-left: auto;
             }
-          }
+
+            ${_breakpointEntries.map(([, value]) => `
+              @media (min-width: ${value}px) {
+                .flex-container {
+                  max-width: ${value}px;
+                }
+              }
+              `).join('\n')}
+            `
+
+          // yield {
+          //   [`--${variablePrefix}gutter-x`]: `${gutter / 2}px`,
+          //   [`--${variablePrefix}gutter-y`]: '0px',
+          //   width: '100%',
+          //   'padding-right': `var(--${variablePrefix}gutter-x, .75rem)`,
+          //   'padding-left': `var(--${variablePrefix}gutter-x, .75rem)`,
+          //   'margin-right': 'auto',
+          //   'margin-left': 'auto'
+          // }
+          // yield {
+          //   [symbols.selector]: selector => `${selector}-fluid`,
+          //   [`--${variablePrefix}gutter-x`]: `${gutter / 2}px`,
+          //   [`--${variablePrefix}gutter-y`]: '0px',
+          //   width: '100%',
+          //   'padding-right': `var(--${variablePrefix}gutter-x, .75rem)`,
+          //   'padding-left': `var(--${variablePrefix}gutter-x, .75rem)`,
+          //   'margin-right': 'auto',
+          //   'margin-left': 'auto'
+          // }
+
+          // for (const [, value] of _breakpointEntries) {
+          //   yield {
+          //     [symbols.parent]: `@media (min-width: ${value}px)`,
+          //     'max-width': value
+              
+          //   }
+          // }
         }
       ],
-      [
-        /^row$/,
-        () => { }
-      ],
-      [
-        /^col-?(\\d*)$/,
-        () => { }
-      ]
+      // [
+      //   /^row$/,
+      //   () => { }
+      // ],
+      // [
+      //   /^col-?(\\d*)$/,
+      //   () => { }
+      // ]
     ]
   }
-})
+}
