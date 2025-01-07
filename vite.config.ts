b6d43a95610d6fa8ea60f2658aaea3232552537c
madao <gn00678465@gmail.com>
2025-01-07 10:05:15 +0800

    build: 修正 build 設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts()
+  ],
   build: {
     lib: {
       entry: './src/index.ts',
-      formats: ['es'],
-      fileName: () => 'index.js'
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
       external: ['unocss']
